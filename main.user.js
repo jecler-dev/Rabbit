037ee879eecfee81a4ce5cbfe176859c3a97ff67
jecler-dev <jecler.dev@gmail.com>
2026-03-17 00:04:32 -0300

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,14 +1,54 @@
 // ==UserScript==
-// @name 🐰 Rabbit Pack PRO
-// @match *://*.tribalwars.com.br/*
+// @name         🐰 Rabbit Pack PRO
+// @namespace    rabbit.pack.pro
+// @version      1.0
+// @match        *://*.tribalwars.com.br/*
+// @grant        none
+
+/* ==================================================
+   CORE
+================================================== */
+
+// @require https://github.com/jecler-dev/Rabbit/raw/refs/heads/main/core/engine.js
+
+
+/* ==================================================
+   INTERFACE
+================================================== */
+
 // @require https://github.com/jecler-dev/Rabbit/raw/refs/heads/main/interface/theme.js
 // @require https://github.com/jecler-dev/Rabbit/raw/refs/heads/main/interface/menu.js
+
+
+/* ==================================================
+   MODULES
+================================================== */
+
+// @require https://github.com/jecler-dev/Rabbit/raw/refs/heads/main/modules/auto_build.js
+
+
 // ==/UserScript==
 
 (function(){
 
 'use strict';
 
+/* ==================================================
+   START
+================================================== */
+
+function startRabbit(){
+
+console.log("🐰 Rabbit Pack iniciado");
+
 RabbitMenu.init();
+
+}
+
+/* ==================================================
+   INIT DELAY (evita bugs do TW)
+================================================== */
+
+setTimeout(startRabbit,2000);
 
 })();
