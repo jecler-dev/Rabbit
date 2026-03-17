d13e5d920fac151f69dd6385b5aac140fabf40f7
jecler-dev <jecler.dev@gmail.com>
2026-03-16 23:50:36 -0300

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name 🐰 Rabbit Pack PRO
 // @match *://*.tribalwars.com.br/*
-// @require https://cdn.jsdelivr.net/gh/SEU_USER/rabbit-pack/interface/theme.js
-// @require https://cdn.jsdelivr.net/gh/SEU_USER/rabbit-pack/interface/menu.js
+// @require https://github.com/jecler-dev/Rabbit/raw/refs/heads/main/interface/theme.js
+// @require https://github.com/jecler-dev/Rabbit/raw/refs/heads/main/interface/menu.js
 // ==/UserScript==
 
 (function(){
