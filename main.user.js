33d1aa0fac759cd686730df5bcb8ca327840a39a
jecler-dev <jecler.dev@gmail.com>
2026-03-16 23:46:13 -0300

    Create main.user.js

---
+++ main.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name 🐰 Rabbit Pack PRO
+// @match *://*.tribalwars.com.br/*
+// @require https://cdn.jsdelivr.net/gh/SEU_USER/rabbit-pack/interface/theme.js
+// @require https://cdn.jsdelivr.net/gh/SEU_USER/rabbit-pack/interface/menu.js
+// ==/UserScript==
+
+(function(){
+
+'use strict';
+
+RabbitMenu.init();
+
+})();
