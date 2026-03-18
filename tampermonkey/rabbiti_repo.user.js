d5e6de20c8bf9336316dfbffd5220ff5b4c8fd14
jecler-dev <jecler.dev@gmail.com>
2026-03-17 22:54:28 -0300

    Update rabbiti_repo.user.js

--- tampermonkey/rabbiti_repo.user.js
+++ tampermonkey/rabbiti_repo.user.js
@@ -1,4 +1,30 @@
 // ==UserScript==
+// @name         RabbitScript - Repo
+// @namespace    https://github.com/seu-user
+// @version      1.0
+// @description  Scripts organizados via @require
+// @author       Rabbit
+// @match        *://*/*
+
+// arquivos do projeto
+// @require https://raw.githubusercontent.com/seu-user/meu-projeto/main/src/utils.js
+// @require https://raw.githubusercontent.com/seu-user/meu-projeto/main/src/file1.js
+// @require https://raw.githubusercontent.com/seu-user/meu-projeto/main/src/file2.js
+// @require https://raw.githubusercontent.com/seu-user/meu-projeto/main/src/file3.js
+
+// atualização automática
+// @updateURL    https://raw.githubusercontent.com/seu-user/meu-projeto/main/main.user.js
+// @downloadURL  https://raw.githubusercontent.com/seu-user/meu-projeto/main/main.user.js
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("Repositorio Carregado");
+})();
+```
+// ==UserScript==
 // @name         Repositorio RabbitScripts
 // @match        *://*/*
 // @require https://raw.githubusercontent.com/user/repo/main/pasta/file1.js
