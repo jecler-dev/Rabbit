25e5f368e54626b609ddc026f53a7c5448abae7e
jecler-dev <jecler.dev@gmail.com>
2026-03-18 01:31:38 -0300

    Update rabbiti_repo.user.js

--- tampermonkey/rabbiti_repo.user.js
+++ tampermonkey/rabbiti_repo.user.js
@@ -1,25 +1,28 @@
+```js
 // ==UserScript==
 // @name         RabbitScript - Repo
-// @namespace    https://github.com/seu-user
+// @namespace    https://github.com/jecler-dev
 // @version      1.0
 // @description  Scripts organizados via @require
 // @author       Rabbit
 // @match        *://*/*
+// @grant        none
 
 // arquivos do projeto
-// @require https://raw.githubusercontent.com/jecler-dev/Rabbit/refs/heads/main/tampermonkey/raio_de_ataque.js
-// @require https://raw.githubusercontent.com/jecler-dev/Rabbit/refs/heads/main/tampermonkey/painel_lateral.js
-// @require https://raw.githubusercontent.com/jecler-dev/Rabbit/refs/heads/main/tampermonkey/barra_plus.js
-// @require https://raw.githubusercontent.com/jecler-dev/Rabbit/refs/heads/main/tampermonkey/Autofarm_V2.js
+// @require https://raw.githubusercontent.com/jecler-dev/Rabbit/main/tampermonkey/raio_de_ataque.js
+// @require https://raw.githubusercontent.com/jecler-dev/Rabbit/main/tampermonkey/painel_lateral.js
+// @require https://raw.githubusercontent.com/jecler-dev/Rabbit/main/tampermonkey/barra_plus.js
+// @require https://raw.githubusercontent.com/jecler-dev/Rabbit/main/tampermonkey/Autofarm_V2.js
 
 // atualização automática
-// @updateURL    https://github.com/jecler-dev/Rabbit/raw/refs/heads/main/tampermonkey/rabbiti_repo.user.js
-// @downloadURL  https://github.com/jecler-dev/Rabbit/raw/refs/heads/main/tampermonkey/rabbiti_repo.user.js
+// @updateURL    https://raw.githubusercontent.com/jecler-dev/Rabbit/main/main.user.js
+// @downloadURL  https://raw.githubusercontent.com/jecler-dev/Rabbit/main/main.user.js
 
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    console.log("Repositorio Carregado");
+    console.log("Repositorio Carregado 🚀");
 })();
+```
