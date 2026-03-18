69d5aff8462badd1c8d7f01939c9efc6d1cb382c
jecler-dev <jecler.dev@gmail.com>
2026-03-17 22:49:13 -0300

    Create rabbiti_repo.user.js

---
+++ tampermonkey/rabbiti_repo.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Meu Projeto
+// @match        *://*/*
+// @require https://raw.githubusercontent.com/user/repo/main/pasta/file1.js
+// @require https://raw.githubusercontent.com/user/repo/main/pasta/file2.js
+// @require https://raw.githubusercontent.com/user/repo/main/pasta/file3.js
+// ... até o 10
+// ==/UserScript==
