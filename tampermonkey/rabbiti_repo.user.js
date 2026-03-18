f282b83f5a6117cbe08a4776883f73a4f4bf09b2
jecler-dev <jecler.dev@gmail.com>
2026-03-18 01:27:59 -0300

    Update rabbiti_repo.user.js

--- tampermonkey/rabbiti_repo.user.js
+++ tampermonkey/rabbiti_repo.user.js
@@ -23,12 +23,3 @@
 
     console.log("Repositorio Carregado");
 })();
-```
-// ==UserScript==
-// @name         Repositorio RabbitScripts
-// @match        *://*/*
-// @require https://raw.githubusercontent.com/user/repo/main/pasta/file1.js
-// @require https://raw.githubusercontent.com/user/repo/main/pasta/file2.js
-// @require https://raw.githubusercontent.com/user/repo/main/pasta/file3.js
-// ... até o 10
-// ==/UserScript==
